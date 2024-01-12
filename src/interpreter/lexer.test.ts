9146072e91e29e4ca48541d0d58ed4c59aa3a416
Nicholas Thanasiu <nicholasthanasiu@Nicholass-MacBook-Pro.local>
2024-01-11 21:08:28 -0500

    Test lexer support for newer tokens >, <, /, *,

--- src/interpreter/lexer.test.ts
+++ src/interpreter/lexer.test.ts
@@ -23,7 +23,21 @@ test(`readChar sets ch to "\0 when finished reading all input`, () => {
 });
 
 test(`nextToken works`, () => {
-  const input = `let five = 5;`;
+  const input = `
+    let five = 5;
+    let ten = 10;
+
+    let add = fn(x, y) {
+      x + y;
+    }
+
+    let result = add(five, ten);
+
+    !-/*5;
+    5 < 10 > 5;
+  `;
+
+  
 
   const expectedTokens: Token[] = [
     { type: TokenType.LET, literal: "let" },
@@ -31,6 +45,54 @@ test(`nextToken works`, () => {
     { type: TokenType.ASSIGN, literal: "=" },
     { type: TokenType.INT, literal: "5" },
     { type: TokenType.SEMICOLON, literal: ";" },
+
+    { type: TokenType.LET, literal: "let" },
+    { type: TokenType.IDENT, literal: "ten" },
+    { type: TokenType.ASSIGN, literal: "=" },
+    { type: TokenType.INT, literal: "10" },
+    { type: TokenType.SEMICOLON, literal: ";"},
+
+    { type: TokenType.LET, literal: "let" },
+    { type: TokenType.IDENT, literal: "add" },
+    { type: TokenType.ASSIGN, literal: "=" },
+    { type: TokenType.FUNCTION, literal: "fn" },
+    { type: TokenType.LPAREN, literal: "(" },
+    { type: TokenType.IDENT, literal: "x"},
+    { type: TokenType.COMMA, literal: ","},
+    { type: TokenType.IDENT, literal: "y"},
+    { type: TokenType.RPAREN, literal: ")" },
+    { type: TokenType.LBRACE, literal: "{" },
+    { type: TokenType.IDENT, literal: "x" },
+    { type: TokenType.PLUS, literal: "+" },
+    { type: TokenType.IDENT, literal: "y" },
+    { type: TokenType.SEMICOLON, literal: ";" },
+    { type: TokenType.RBRACE, literal: "}" },
+
+    { type: TokenType.LET, literal: "let" },
+    { type: TokenType.IDENT, literal: "result" },
+    { type: TokenType.ASSIGN, literal: "=" },
+    { type: TokenType.IDENT, literal: "add" },
+    { type: TokenType.LPAREN, literal: "(" },
+    { type: TokenType.IDENT, literal: "five" },
+    { type: TokenType.COMMA, literal: "," },
+    { type: TokenType.IDENT, literal: "ten" },
+    { type: TokenType.RPAREN, literal: ")" },
+    { type: TokenType.SEMICOLON, literal: ";" },
+
+    { type: TokenType.BANG, literal: "!" },
+    { type: TokenType.MINUS, literal: "-" },
+    { type: TokenType.SLASH, literal: "/" },
+    { type: TokenType.ASTERISK, literal: "*" },
+    { type: TokenType.INT, literal: "5" },
+    { type: TokenType.SEMICOLON, literal: ";" },
+
+    { type: TokenType.INT, literal: "5" },
+    { type: TokenType.LT, literal: "<" },
+    { type: TokenType.INT, literal: "10" },
+    { type: TokenType.GT, literal: ">" },
+    { type: TokenType.INT, literal: "5" },
+    { type: TokenType.SEMICOLON, literal: ";" },
+
     { type: TokenType.EOF, literal: "\0" },
   ];
 
