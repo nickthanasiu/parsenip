61d11f625c0efd09cf5dcbc207837e3175b732e9
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2024-06-06 19:35:59 -0400

    Fix full screen bug; Add icons

--- src/features/panels/Panel.tsx
+++ src/features/panels/Panel.tsx
@@ -26,6 +26,14 @@ export function Panel({
   const panelRef = useRef<HTMLDivElement>(null);
   const workspaceCtx = useContext(WorkspaceCtx);
 
+  function expandToFullScreen(e: React.MouseEvent<HTMLButtonElement>) {
+    if (!expanded) {
+      toggleExpanded(e);
+    }
+    
+    workspaceCtx.fullScreen?.set(panelId);
+  }
+
   const isFullScreen = panelId === workspaceCtx.fullScreen?.panelId;
 
   const panelClasses = useClasses({
@@ -67,15 +75,15 @@ export function Panel({
             {isFullScreen
               ? 
                 <button onClick={() => workspaceCtx.fullScreen?.close()}>
-                  -
+                  <Icon type="compress" />
                 </button>
               :
                 <>
-                  <button onClick={() => workspaceCtx.fullScreen?.set(panelId)}>
-                    F
+                  <button onClick={expandToFullScreen}>
+                    <Icon type="expand" />
                   </button>
                   <button onClick={toggleExpanded}>
-                    <Arrow orientation={
+                    <ArrowIcon orientation={
                       vertical
                         ? expanded ? 'up' : 'down'
                         : expanded ? 'left' : 'up'
@@ -94,24 +102,50 @@ export function Panel({
   );
 }
 
+type IconType =
+  | 'expand'
+  | 'compress'
+  | 'arrow'
 
-  interface ArrowProps {
-    orientation: 'up' | 'down' | 'left' | 'right';
-  }
+interface IconProps {
+  type: IconType;
+  width?: string;
+  style?: React.CSSProperties;
+}
 
-  function Arrow({ orientation }: ArrowProps) {
-    const mOrientationToDeg = {
-      up: 0,
-      right: 90,
-      down: 180,
-      left: 270,
-    };
+const iconPaths: Record<IconType, string> = {
+  expand: "M136 32c13.3 0 24 10.7 24 24s-10.7 24-24 24H48v88c0 13.3-10.7 24-24 24s-24-10.7-24-24V56C0 42.7 10.7 32 24 32H136zM0 344c0-13.3 10.7-24 24-24s24 10.7 24 24v88h88c13.3 0 24 10.7 24 24s-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V344zM424 32c13.3 0 24 10.7 24 24V168c0 13.3-10.7 24-24 24s-24-10.7-24-24V80H312c-13.3 0-24-10.7-24-24s10.7-24 24-24H424zM400 344c0-13.3 10.7-24 24-24s24 10.7 24 24V456c0 13.3-10.7 24-24 24H312c-13.3 0-24-10.7-24-24s10.7-24 24-24h88V344z",
+  compress: "M160 56c0-13.3-10.7-24-24-24s-24 10.7-24 24v88H24c-13.3 0-24 10.7-24 24s10.7 24 24 24H136c13.3 0 24-10.7 24-24V56zM24 320c-13.3 0-24 10.7-24 24s10.7 24 24 24h88v88c0 13.3 10.7 24 24 24s24-10.7 24-24V344c0-13.3-10.7-24-24-24H24zM336 56c0-13.3-10.7-24-24-24s-24 10.7-24 24V168c0 13.3 10.7 24 24 24H424c13.3 0 24-10.7 24-24s-10.7-24-24-24H336V56zM312 320c-13.3 0-24 10.7-24 24V456c0 13.3 10.7 24 24 24s24-10.7 24-24V368h88c13.3 0 24-10.7 24-24s-10.7-24-24-24H312z",
+  arrow: "M15 239c-9.4 9.4-9.4 24.6 0 33.9L207 465c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9L65.9 256 241 81c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0L15 239z",
+};
 
-    return (
-      <div style={{
-        transform: `rotate(${mOrientationToDeg[orientation]}deg)`
-      }}>
-        ^
-      </div>
-    );
-  }
+function Icon({ type, width, style }: IconProps) {
+  return (
+    <svg
+      width={width ?? '10px'}
+      style={style}
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+      >
+        <path d={iconPaths[type]}></path>
+    </svg>
+  );
+}
+
+interface ArrowProps {
+  orientation: 'up' | 'down' | 'left' | 'right';
+}
+
+function ArrowIcon({ orientation }: ArrowProps) {
+  const mOrientationToDeg = {
+    left: 0,
+    up: 90,
+    right: 180,
+    down: 270,
+  };
+
+  return (
+    <Icon type="arrow" style={{ transform: `rotate(${mOrientationToDeg[orientation]}deg)` }} />
+  );
+}
